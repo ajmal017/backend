5365d0050b0c94e33c62245344e63faf16cffbdf
Rashmi Mittal <rashmi@finaskus.com>
2016-09-13 12:45:45 +0530

    Try to remove circular dependency...

--- webapp/conf/aws_settings.py
+++ webapp/conf/aws_settings.py
@@ -3,6 +3,7 @@ import os
 USING_S3 = True
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_S3_SECURE_URLS = False
 AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
