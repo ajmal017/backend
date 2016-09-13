97d906fd3641ef68141c08df458d534532136530
Rashmi Mittal <rashmi@finaskus.com>
2016-09-13 15:02:16 +0530

    Fix mandate file name and add bcc to appadmin

--- webapp/apps/profiles/helpers.py
+++ webapp/apps/profiles/helpers.py
@@ -303,10 +303,10 @@ def send_transaction_change_email(order_detail,applicant_name,user,email_attachm
         subject = loader.render_to_string('transaction/user-status-change-subject.txt', context)
         subject = ''.join(subject.splitlines())
         body = loader.render_to_string(html_email_template_name, context)
-        email_message = EmailMultiAlternatives(subject, body, from_email, [user.email])
+        email_message = EmailMultiAlternatives(subject, body, from_email, [user.email], bcc=settings.DEFAULT_TO_EMAIL)
         if user.mandate_status == "0" and email_attachment is not None:
             attachment = open(email_attachment, 'rb')
-            email_message.attach(os.path.basename(email_attachment), attachment.read(),'application/pdf')
+            email_message.attach('bank_mandate.pdf', attachment.read(),'application/pdf')
             user.mandate_status = 1
             user.save()
         email_message.attach_alternative(body, 'text/html')
