7a658574524dd2219a9238d9a69cddb373be722d
codetoend <adityagoyal619@gmail.com>
2016-09-20 02:51:37 +0530

    Added changes for dynamic request body and xpaths constants

--- webapp/apps/external_api/nse/create_validate_nserequests.py
+++ webapp/apps/external_api/nse/create_validate_nserequests.py
@@ -1,4 +1,4 @@
-
+import xml.etree.ElementTree as ET
 
 from profiles import models
 from external_api.models import Pincode
@@ -30,100 +30,23 @@ def getiinrequest(root, user_id):
 
 
     investor_dict={
-     "ACNo" : investor_bank.account_number,
-     "AcType" : investor_bank.account_type,
-     "BankAddress" : investor_bank.ifsc_code.address,
-     "BankCity" : investor_bank.ifsc_code.city,
-     "BankCountry" : constants.INDIA,
-     "BankPincode" : None,
-     "BankState" : investor_bank.ifsc_code.state,
-     "Branch" : investor_bank.ifsc_code.bank_branch,
-     "BrokerAgentCodeARN" : "108537",
-     "City" : contact.communication_address.pincode.city,
-     "ContactAddress" : contact.communication_address.address_line_1,
-     "Country" : constants.INDIA,
-     "CountryofTaxResidence" : constants.INDIA,
-     "CountryofTaxResidence_2" : None,
-     "CountryofTaxResidence_3" : None,
-     "DateofBirth" : investor.dob.strftime('%d-%m-%Y'),
-     "DateofBirth_2" : None,
-     "DateofBirth_3" : None,
-     "EUIN" : "E148376",
-     "Email" : contact.email,
-     "FatherName" : investor.father_name,
-     "FaxOff" : None,
-     "FaxRes" : None,
-     "GuardianNameIfNomineeMinor" : nominee.guardian_name if nominee else None,
-     "GuardianPAN" : None,
-     "IFSCCode" : investor_bank.ifsc_code.ifsc_code,
-     "IncomeTaxSlabNetworth" : None,  # TODO
-     "IncomeTaxSlabNetworth_2" : None,
-     "IncomeTaxSlabNetworth_3" : None,
-     "KYC" : investor.kra_verified,
-     "KYC_2" : None,
-     "KYC_3" : None,
-     "Mobile" : contact.phone_number,
-     "ModeofHolding" : None,  # TODO
-     "MotherName" : None,  # TODO
-     "NameofThirdApplicant" : None,
-     "NameFirstApplicant" : investor.applicant_name,
-     "NameGuardian" : nominee.guardian_name if nominee else None,
-     "NameofBank" : investor_bank.ifsc_code.name,
-     "NameofSecondApplicant" : None,
-     "NomineeAddress" : nominee.nominee_address.address_line_1,
-     "NomineeCity" : nominee.nominee_address.pincode.city,
-     "NomineeName" : nominee.nominee_name if nominee else None,
-     "NomineePincode" : nominee.nominee_address.pincode.pincode,
-     "NomineeRelationship" : nominee.get_relationship_with_investor_display() if nominee else None,
-     "NomineeState" : nominee.nominee_address.pincode.state,
-     "Occupation" : investor.get_occupation_type_display() if investor.occupation_type != "OTH" else investor.occupation_specific,
-     "OccupationDetails" : None,
-     "OccupationDetails_2" : None,
-     "OccupationDetails_3" : None,
-     "OverseasAddress" : None,
-     "OverseasCity" : None,
-     "OverseasCountry" : None,
-     "OverseasPincode" : None,
-     "PANNumber" : investor.pan_number,
-     "PANNumber_2" : None,
-     "PANNumber_3" : None,
-     "Pincode" : contact.communication_address.pincode.pincode,
-     "PlaceofBirth" : None,
-     "PlaceofBirth_2" : None,
-     "PlaceofBirth_3" : None,
-     "PoliticallyExposedNo" : False if investor.political_exposure == 2 else True,
-     "PoliticallyExposedNo_2" : None,
-     "PoliticallyExposedNo_3" : None,
-     "PoliticallyExposedYes" : True if investor.political_exposure == 2 else False,
-     "PoliticallyExposedYes_2" : None,
-     "PoliticallyExposedYes_3" : None,
-     "SUBBROKER" : None,
-     "SignatureDate" : curr_date.strftime('%d-%m-%Y'),
-     "SignaturePlace" : contact.communication_address.pincode.city,
-     "State" : contact.communication_address.pincode.state,
-     "TaxIdNo" : None,  # TODO
-     "TaxIdNo_2" : None,
-     "TaxIdNo_3" : None,
-     "TelOff" : None,
-     "TelRes": None
+     constants.TAX_STATUS_XPATH : investor_bank.account_number,
+     constants.HOLD_NATURE_XPATH:None,
+     constants.EXEMPT_FLAG_XPATH:None,
+     constants.FH_PAN_XPATH:None,
+     constants.JH1_PAN_XPATH:None,
+     constants.JH1_EXEMPT_FLAG_XPATH:None,
+     constants.JH2_PAN_XPATH:None,
+     constants.JH2_EXEMPT_FLAG_XPATH:None,
+     constants.GUARD_PAN_XPATH:None
      }
 
     for key, value in investor_dict.items():
         if value is None:
             investor_dict[key] = ""
-        if value is True:
-            investor_dict[key] = "Yes"
-        if value is False:
-            investor_dict[key] = "Off"
+        root.find(key).text = value
 
-    for key, value in investor_dict.items():
-        if value not in ("", "Yes", "Off"):
-            if type(value) == str:
-                if "@" in value:
-                    continue
-            investor_dict[key] = str(value).upper()
-
-    return
+    return root
 
 
 def createcustomerrequest(root, user_id):
@@ -145,7 +68,71 @@ def createcustomerrequest(root, user_id):
         blank_address = models.Address(None, None, None, None)
         blank_address.pincode = blank_pincode
         nominee.nominee_address = blank_address
-    return
+
+    investor_dict={
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+        constants: None,
+     }
+
+    for key, value in investor_dict.items():
+        if value is None:
+            investor_dict[key] = ""
+        root.find(key).text = value
+
+    return root
 
 
 def purchasetxnrequest(root, user_id):
@@ -167,7 +154,93 @@ def purchasetxnrequest(root, user_id):
         blank_address = models.Address(None, None, None, None)
         blank_address.pincode = blank_pincode
         nominee.nominee_address = blank_address
-    return
+
+
+    investor_dict={
+        constants.IIN_XPATH:None,
+        constants.SUB_TRXN_TYPE_XPATH:None,
+        constants.POA_XPATH:None,
+        constants.TRXN_ACCEPTANCE_XPATH:None,
+        constants.DEMAT_USER_XPATH:None,
+        constants.DP_ID_XPATH:None,
+        constants.BANK_NAME_XPATH:None,
+        constants.AC_NO_XPATH:None,
+        constants.IFSC_CODE_XPATH:None,
+        constants.SUB_BROKER_ARN_CODE_XPATH:None,
+        constants.SUB_BROKER_CODE_XPATH:None,
+        constants.EUIN_OPTED_XPATH:None,
+        constants.TRXN_EXECUTION_XPATH:None,
+        constants.REMARKS_XPATH:None,
+        constants.PAYMENT_MODE_XPATH:None,
+        constants.BILLDESK_BANK_XPATH:None,
+        constants.INSTRM_BANK_XPATH:None,
+        constants.INSTRM_AC_NO_XPATH:None,
+        constants.INSTRM_NO_XPATH:None,
+        constants.INSTRM_AMOUNT_XPATH:None,
+        constants.INSTRM_DATE_XPATH:None,
+        constants.INSTRM_BRANCH_XPATH:None,
+        constants.INSTRM_CHARGES_XPATH:None,
+        constants.MICR_XPATH:None,
+        constants.RTGS_CODE_XPATH:None,
+        constants.NEFT_IFSC_XPATH:None,
+        constants.ADVISIORY_CHARGE_XPATH:None,
+        constants.CHEQUE_DEPOSIT_MODE_XPATH:None,
+        constants.DD_CHARGE_XPATH:None,
+        constants.DEBIT_AMOUNT_TYPE_XPATH:None,
+        constants.NOMINEE_FLAG_XPATH:None,
+        constants.NO_OF_NOMINEE_XPATH:None,
+        constants.NOMINEE1_NAME_XPATH:None,
+        constants.NOMINEE1_DOB_XPATH:None,
+        constants.NOMINEE1_ADDR1_XPATH:None,
+        constants.NOMINEE1_ADDR2_XPATH:None,
+        constants.NOMINEE1_ADDR3_XPATH:None,
+        constants.NOMINEE1_CITY_XPATH:None,
+        constants.NOMINEE1_STATE_XPATH:None,
+        constants.NOMINEE1_PINCODE_XPATH:None,
+        constants.NOMINEE1_RELATION_XPATH:None,
+        constants.NOMINEE1_PERCENT_XPATH:None,
+        constants.NOMINEE1_GUARD_NAME_XPATH:None,
+        constants.NOMINEE1_GUARD_PAN_XPATH:None,
+        constants.NOMINEE2_NAME_XPATH:None,
+        constants.NOMINEE2_DOB_XPATH:None,
+        constants.NOMINEE2_RELATION_XPATH:None,
+        constants.NOMINEE2_PERCENT_XPATH:None,
+        constants.NOMINEE2_GUARD_NAME_XPATH:None,
+        constants.NOMINEE3_NAME_XPATH:None,
+        constants.NOMINEE3_DOB_XPATH:None,
+        constants.NOMINEE3_RELATION_XPATH:None,
+        constants.NOMINEE3_PERCENT_XPATH:None,
+        constants.NOMINEE3_GUARD_NAME_XPATH:None,
+        constants.NOMINEE3_GUARD_PAN_XPATH:None,
+        constants.SIP_MICR_NO_XPATH:None,
+        constants.SIP_BANK_XPATH:None,
+        constants.SIP_ACC_NO_XPATH:None,
+        constants.SIP_AC_TYPE_XPATH:None,
+        constants.SIP_IFSC_CODE_XPATH:None,
+        constants.UMRN_XPATH:None,
+        constants.ACH_AMT_XPATH:None,
+        constants.ACH_FROM_DATE_XPATH:None,
+        constants.ACH_END_DATE_XPATH:None,
+        constants.UNTIL_CANCELLED_XPATH:None,
+        constants.RETURN_PAYMENT_FLAG_XPATH:None,
+        constants.CLIENT_CALLBACK_URL_XPATH:None,
+        constants.TRANS_COUNT_XPATH:None,
+        constants.AMC_XPATH:None,
+        constants.FOLIO_XPATH:None,
+        constants.AMOUNT_XPATH:None,
+        constants.SIP_FROM_DATE_XPATH:None,
+        constants.SIP_END_DATE_XPATH:None,
+        constants.SIP_FREQ_XPATH:None,
+        constants.SIP_AMOUNT_XPATH:None,
+        constants.SIP_PERIOD_DAY_XPATH:None
+     }
+
+    for key, value in investor_dict.items():
+        if value is None:
+            investor_dict[key] = ""
+        root.find(key).text = value
+
+    return root
 
 
 def achmandateregistrationsrequest(root, user_id):
@@ -189,4 +262,23 @@ def achmandateregistrationsrequest(root, user_id):
         blank_address = models.Address(None, None, None, None)
         blank_address.pincode = blank_pincode
         nominee.nominee_address = blank_address
-    return
+
+    investor_dict={
+        constants.IIN_XPATH:None,
+        constants.ACC_NO_XPATH:None,
+        constants.ACC_TYPE_XPATH:None,
+        constants.IFSC_CODE_XPATH:None,
+        constants.BANK_NAME_XPATH:None,
+        constants.MICR_NO_XPATH:None,
+        constants.UC_XPATH:None,
+        constants.ACH_FROM_DATE_XPATH:None,
+        constants.ACH_TO_DATE_XPATH:None,
+        constants.ACH_AMOUNT_XPATH:None
+     }
+
+    for key, value in investor_dict.items():
+        if value is None:
+            investor_dict[key] = ""
+        root.find(key).text = value
+
+    return root
