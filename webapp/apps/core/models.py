98df205f885cbaddde552587c48dd28644adcd0d
Rashmi Mittal <rashmi@finaskus.com>
2016-09-23 10:44:03 +0530

    Remove log statement

--- webapp/apps/core/models.py
+++ webapp/apps/core/models.py
@@ -119,8 +119,6 @@ def order_detail_transaction_mail_send(order_detail):
                 investment_date = portfolio.investment_date
                 if datetime.datetime.date(order_detail.created_at) > investment_date:
                     order_info,applicant_name,order_detail.user,email_attachment,attachment_error,sip_tenure,goal_tenure_len = order_detail_info_function(order_detail,portfolio)
-                    logger = logging.getLogger('django.debug')
-                    logger.debug('SIP completed len: ' + str(len(order_info)))
                     first_order = False
                     msg = profile_helpers.send_transaction_change_email(first_order,order_info,applicant_name,order_detail.user,email_attachment,attachment_error,sip_tenure,goal_tenure_len,use_https=settings.USE_HTTPS)
                     if msg == "success":
