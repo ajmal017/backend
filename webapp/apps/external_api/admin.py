dd66b5e03669fc3a82e98fac08cfbc7a36ca0a01
Rashmi Mittal <rashmi@finaskus.com>
2016-10-05 12:51:17 +0530

    Fix VendorHelper

--- webapp/apps/external_api/admin.py
+++ webapp/apps/external_api/admin.py
@@ -49,6 +49,10 @@ class VendorAdmin(admin.ModelAdmin):
     list_display = ['name', 'active']
     list_editable = ['active']
     filter = ['name']
+    actions = None
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 admin.site.register(models.SMS, SMSAdmin)
 admin.site.register(models.VerifiablePincode, VerifiablePincodeAdmin)
