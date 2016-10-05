2ee252e68a74403b1689a1774676bedd2524f656
Rashmi Mittal <rashmi@finaskus.com>
2016-10-05 18:14:29 +0530

    Fix error logging

--- webapp/apps/external_api/nse/create_validate_nserequests.py
+++ webapp/apps/external_api/nse/create_validate_nserequests.py
@@ -39,7 +39,7 @@ def getValidRequestWithMultipleChildren(investor_dict, child_items, root, child_
     return ET.tostring(root, encoding="us-ascii", method="xml")
 
 def changeDobFormat(dob):
-    return dob.strftime('%m/%d/%Y')
+    return dob.strftime('%d-%b-%Y')
 
 def get_amc_code(code):
     AMC_CODE_MAP = {
