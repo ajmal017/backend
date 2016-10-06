0f7673e76e36ec21142323c3f25c6ffeac02438a
Rashmi Mittal <rashmi@finaskus.com>
2016-10-06 17:42:18 +0530

    Fix mising data in iin form

--- webapp/apps/external_api/nse/nsebackend.py
+++ webapp/apps/external_api/nse/nsebackend.py
@@ -337,5 +337,5 @@ class NSEBackend(ExchangeBackend):
                 
     def create_redeem(self, user_id, grouped_redeem):
         return self.redeem_trxn(user_id, grouped_redeem), None
-    
+
     
