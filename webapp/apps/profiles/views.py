8118b5a6cb2c355c77e3dbeac960647e42d9d924
Rashmi Mittal <rashmi@finaskus.com>
2016-12-23 09:56:46 +0530

    Accept data uri for signature

--- webapp/apps/profiles/views.py
+++ webapp/apps/profiles/views.py
@@ -37,7 +37,8 @@ import logging
 import warnings
 import logging
 import threading
-
+import re
+import base64
 
 from django.core.files.base import ContentFile
 import urllib.request as urllib2
@@ -799,22 +800,36 @@ class SaveImage(APIView):
                 return api_utils.response({"message": constants.SUCCESS, "permanent_back_image": contact.permanent_back_image.url,"permanent_back_image_thumbnail": contact.permanent_back_image_thumbnail.url}, status.HTTP_200_OK)
             return api_utils.response({}, status.HTTP_400_BAD_REQUEST, generate_error_message(serializer.errors))
 
-        elif request.data.get('signature', None):
+        elif request.data.get('signature', None) or request.data.get('signature_data', None):
             """
             This method accepts a image file and updates the signature image.
 
             :return: 'Signature saved' response on successful image store.
             """
-
-            serializer = serializers.SaveSignatureSerializer(request.user, data=request.data)
-            if serializer.is_valid():
-                request.user.signature = request.FILES.get('signature', None)
+            is_valid = True
+            if (request.POST['signature_data']):
+                try:
+                    image_data = re.search(r'base64,(.*)', request.POST['signature_data']).group(1)
+                    signature_data = base64.b64decode(image_data)
+                    signature_file = ContentFile(signature_data, 'signature.png')
+                    request.user.signature = signature_file
+                except:
+                    is_valid = False
+            else:    
+                serializer = serializers.SaveSignatureSerializer(request.user, data=request.data)
+                if serializer.is_valid():
+                    request.user.signature = request.FILES.get('signature', None)
+                else:
+                    is_valid = False
+                
+            if is_valid:
                 request.user.finaskus_id = core_utils.get_finaskus_id(request.user)
                 request.user.save()
                 helpers.send_vault_completion_email(request.user, request.user.email, use_https=settings.USE_HTTPS)
                 return api_utils.response({"message": constants.SIGNATURE_SAVED,
                                            "signature": request.user.signature.url}, status.HTTP_200_OK)
             return api_utils.response({}, status.HTTP_404_NOT_FOUND, generate_error_message(serializer.errors))
+
 
         elif request.data.get('nominee_signature', None):
             """
